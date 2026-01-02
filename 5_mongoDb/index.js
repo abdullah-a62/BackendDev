8baf8fbe7878ad27c451afe2d2b45eb08ebeb7b0
qaisirnaseer302 <qaisirnaseer302@gmail.com>
2026-01-02 18:04:50 +0500

    updated

--- 5_mongoDb/index.js
+++ 5_mongoDb/index.js
@@ -1,15 +1,42 @@
-const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://qaisirnaseerDataBase:qaisirnaseerDataBase302@cluster01.iyz0raz.mongodb.net/test?retryWrites=true&w=majority").then(()=>{
-    console.info("database connected")
-}).catch((error)=>{
-    console.error(error)
-});
-// const userSchema=new mongoose.Schema({
-//     name:String,
-//     age:Number,
-//     email:String,
-//     isActive:true,
-//     tags:[String],
-//     currDate:Date.now(),
-// });
-// const user=new mongoose.model("User",userSchema);
+const mongoose=require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/mongoBasics').then(()=>{
+    console.log('database connected');
+}).catch((e)=>{
+    console.log(e);
+})
+const userSchema=new mongoose.Schema({
+    name:String,
+    email:String,
+    age:Number,
+    city:String,
+    tags:[String],
+    currDate:{type:Date,default:Date.now}
+})
+const User=mongoose.model("User",userSchema);
+// create user
+async function runQueries() {
+    try{
+     const newUser=await User.create({
+        name:'ali2',
+        email:'example23@.comc',
+        age:22,
+        city:'multan',
+        tags:['dev','ops'],
+     })
+    console.log('new user created',newUser);
+    const allusers=await User.find({});
+    const specificUser=await User.find({age:22})
+    const findFirstOne=await User.findOne({city:'Multan'});
+    // console.log(findById);
+    // console.log(allusers)
+    const deleteduser=await User.findByIdAndDelete(newUser._id);
+    // console.log(deleteduser)
+    const updateuser=await User.findByIdAndUpdate(newUser._id,{
+        $set : {name:'abdullah'},$push:{tags:'updated'}
+    },{new:true});
+    // console.log(`updated user ${updateuser}`)
+    }catch(e){
+        console.log(`error ${e}`)
+    }
+}
+runQueries();
