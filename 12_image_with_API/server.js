db00b87061996e09ddbc5e58da30b4cf3cd18016
qaisirnaseer302 <qaisirnaseer302@gmail.com>
2026-01-08 02:35:11 +0500

    changes updated

--- 12_image_with_API/server.js
+++ 12_image_with_API/server.js
@@ -1,11 +1,14 @@
 const express=require('express');
-const multer=require('multer');
 const router=require('./routes/route')
+const path=require('path');
+const cors=require('cors');
 const app=express();
 const PORT=3000;
 const connectDb=require('./database/db');
+app.use(cors());
+app.use('./uploads',express.static(path.join(__dirname,'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 connectDb();
-app.use('/api/images/',router);
+app.use('/api/images',router);
 app.listen(PORT)
--- 8b_file_upload/cloudinary.js
+++
@@ -1,7 +0,0 @@
-const cloudinary=require('cloudinary').v2;
-cloudinary.config({
-    cloud_name:'dkgdshsjf',
-    api_key:'389627238989448',
-    api_secret:'geRzNhBcC8HMcoIXh61xbJFd1m0'
-})
-module.exports=cloudinary;
